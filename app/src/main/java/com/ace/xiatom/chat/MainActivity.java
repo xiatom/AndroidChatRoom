dff2397ac39fc79d52123bf5cec27f6dfaf35c73
xiatom <xiatom@126.com>
2019-04-23 22:04:47 +0800

    能够接收以及发送信息

--- app/src/main/java/com/ace/xiatom/chat/MainActivity.java
+++ app/src/main/java/com/ace/xiatom/chat/MainActivity.java
@@ -1,10 +1,13 @@
 package com.ace.xiatom.chat;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import org.jivesoftware.smack.AbstractXMPPConnection;
 import org.jivesoftware.smack.ConnectionConfiguration;
@@ -22,32 +25,22 @@ import java.net.InetAddress;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final int UPDATE_TEXT = 1;
     AbstractXMPPConnection xmpptcpConnection;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        Button btn2 = findViewById(R.id.init);
-        btn2.setOnClickListener(new View.OnClickListener() {
+        final TextView textView = findViewById(R.id.content);
+        final Handler handler = new Handler(){
             @Override
-            public void onClick(View view) {
-
-                try{
-                    Log.i("msg","发送消息");
-                    ChatManager chatManager  = ChatManager.getInstanceFor(xmpptcpConnection);
-                    EntityBareJid jid = JidCreate.entityBareFrom("bubble@10.241.107.22");
-                    Chat chat = chatManager.chatWith(jid);
-                    chat.send("nihao");
-                    chat.send("你好，我是夏冰");
-                    Log.i("msg", chat.getXmppAddressOfChatPartner().toString());
-                }catch (Exception e){
-                    e.printStackTrace();
+            public void handleMessage(android.os.Message msg) {
+               switch (msg.what){
+                   case UPDATE_TEXT:
+                       textView.setText(msg.obj.toString());
                 }
             }
-        });
-
+        };
         Button btn = findViewById(R.id.send);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,8 +50,9 @@ public class MainActivity extends AppCompatActivity {
                     ChatManager chatManager  = ChatManager.getInstanceFor(xmpptcpConnection);
                     EntityBareJid jid = JidCreate.entityBareFrom("ace@localhost");
                     Chat chat = chatManager.chatWith(jid);
-                    chat.send("nihao");
-                    chat.send("你好，我是夏冰");
+                    EditText msg = findViewById(R.id.sendMsg);
+                    String message = msg.getText().toString();
+                    chat.send(message);
                     Log.i("msg", chat.getXmppAddressOfChatPartner().toString());
                 }catch (Exception e){
                     e.printStackTrace();
@@ -75,7 +69,6 @@ public class MainActivity extends AppCompatActivity {
 //                    builder.setXmppDomain(Inet4Address.getLocalHost().getHostAddress());
                     builder.setXmppDomain("localhost");
                     builder.setHostAddress(InetAddress.getByName("10.240.252.96"));
-
                     builder.setPort(5222);
                     builder.setSecurityMode(ConnectionConfiguration.SecurityMode.disabled);
                     builder.setCompressionEnabled(true);
@@ -93,18 +86,30 @@ public class MainActivity extends AppCompatActivity {
                     //设置在线
                     xmpptcpConnection.sendStanza(presence);
                     xmpptcpConnection.login("ace", "1023");
-                    ChatManager chatManager  = ChatManager.getInstanceFor(xmpptcpConnection);
-                    chatManager.addIncomingListener(new IncomingChatMessageListener() {
-                        @Override
-                        public void newIncomingMessage(EntityBareJid from, Message message, Chat chat) {
-                            System.out.println("新消息，来自"+from+":"+message.getBody());
-                        }
-                    });
+
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
         }).start();
+
+
+        try{
+            Thread.sleep(3000);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        ChatManager chatManager  = ChatManager.getInstanceFor(xmpptcpConnection);
+        chatManager.addIncomingListener(new IncomingChatMessageListener() {
+            @Override
+            public void newIncomingMessage(EntityBareJid from, Message message, Chat chat) {
+                android.os.Message msg = new android.os.Message();
+                msg.what = UPDATE_TEXT;
+                msg.obj = "新消息，来自"+from+":"+message.getBody();
+                handler.sendMessage(msg);
+                System.out.println("新消息，来自"+from+":"+message.getBody());
+            }
+        });
 
     }
 }
